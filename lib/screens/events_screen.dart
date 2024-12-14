fd1a4c24ed017eb718e282d9beb81fb49c0f0650
st187986 <st187986@stud.uni-stuttgart.de>
2024-12-14 23:47:55 +0100

    Added refresh feature

--- lib/screens/events_screen.dart
+++ lib/screens/events_screen.dart
@@ -2,8 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:konfus_app/services/event_service.dart';
 import 'package:konfus_app/models/event_model.dart';
 
-class EventsScreen extends StatelessWidget {
+class EventsScreen extends StatefulWidget {
+  const EventsScreen({super.key});
+
+  @override
+  EventsScreenState createState() => EventsScreenState();
+}
+
+class EventsScreenState extends State<EventsScreen> {
   final EventService eventService = EventService();
+  late Future<List<Event>> _eventsFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchEvents();
+  }
+
+  void _fetchEvents() {
+    setState(() {
+      _eventsFuture = eventService.fetchEvents();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,31 +32,38 @@ class EventsScreen extends StatelessWidget {
         title: const Text('Aktionsabende'),
       ),
       body: FutureBuilder<List<Event>>(
-          future: eventService.fetchEvents(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            } else if (snapshot.hasError) {
-              return Center(child: Text('Error: ${snapshot.error}'));
-            } else if (snapshot.hasData) {
-              final events = snapshot.data!;
-              return ListView.builder(
+        future: _eventsFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (snapshot.hasData) {
+            final events = snapshot.data!;
+            return RefreshIndicator(
+              onRefresh: () async {
+                _fetchEvents();
+                await _eventsFuture; // Wait for the fetch to complete
+              },
+              child: ListView.builder(
                 itemCount: events.length,
                 itemBuilder: (context, index) {
                   final event = events[index];
                   String date =
-                      "${DateTime.parse(event.date).day}.${DateTime.parse(event.date).month.toString()}.${DateTime.parse(event.date).year.toString()}";
+                      "${DateTime.parse(event.date).day}.${DateTime.parse(event.date).month}.${DateTime.parse(event.date).year}";
                   return ListTile(
                     leading: Icon(Icons.event),
                     title: Text(event.title),
                     subtitle: Text(date),
                   );
                 },
-              );
-            } else {
-              return Center(child: Text('No Events available'));
-            }
-          }),
+              ),
+            );
+          } else {
+            return Center(child: Text('No Events available'));
+          }
+        },
+      ),
     );
   }
 }
